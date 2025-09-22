5513e083b6c976e3012bc3740a31168d3cca9bc5
LAPTOP-2BL5PO8I\buutruong <truongchanbuu0512@gmail.com>
2025-09-22 16:52:33 +0700

    fix: student schema validation mismatch & add .env.example

--- backend/src/models/student.schema.ts
+++ backend/src/models/student.schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 import { PhoneSchema } from "./phone.model";
 
 export const AddStudentSchema = z.object({
-    username: z.string().min(3).max(10),
+    username: z.string().min(3).max(100),
     phoneNumber: PhoneSchema,
     email: z.email(),
     instructor: PhoneSchema,
